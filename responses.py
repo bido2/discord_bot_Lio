334beb917e848e30b63387e20f226e452a26041f
wiktor <wblaeznek@gmail.com>
2024-11-03 00:17:34 +0100

    cosmetic changes

--- responses.py
+++ responses.py
@@ -5,12 +5,8 @@ from weather_api import return_weather_info
 def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
 
-    if lowered == '':
-        return 'Well, you\'re awfully silent...'
-    elif 'hello' in lowered:
+    if 'hello' in lowered:
         return 'Hello there!'
-    elif 'how are you' in lowered:
-        return 'Good, thanks!'
     elif 'bye' in lowered:
         return 'See you!'
     elif 'roll dice' in lowered:
@@ -20,6 +16,4 @@ def get_response(user_input: str) -> str:
         weather_info = return_weather_info(lowered[lowered.index('temp') + 1])
         return weather_info
     else:
-        return choice(['I do not understand...',
-                       'What are you talking about?',
-                       'Do you mind rephrasing that?'])
+        pass
