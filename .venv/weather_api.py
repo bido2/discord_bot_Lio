9856dabc6a24ec55b4e2bd9c750e260b3c9c103e
wiktor <wblaeznek@gmail.com>
2024-11-02 23:35:57 +0100

    weather api url

---
+++ .venv/weather_api.py
@@ -0,0 +1,3 @@
+import requests
+
+base_url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Zasole?unitGroup=metric&key=NDPVCQ2HDSXVDQPMYNMHBVVVL&contentType=json'
