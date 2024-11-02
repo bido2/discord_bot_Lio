53c5233a370b152968f42377b674a81032c4af38
wiktor <wblaeznek@gmail.com>
2024-11-02 23:47:09 +0100

    update responses with weather option

--- responses.py
+++ responses.py
@@ -1,4 +1,5 @@
 from random import choice, randint
+from weather_api import return_weather_info
 
 
 def get_response(user_input: str) -> str:
@@ -14,6 +15,9 @@ def get_response(user_input: str) -> str:
         return 'See you!'
     elif 'roll dice' in lowered:
         return f'You rolled: {randint(1, 6)}'
+    elif 'weather' in lowered:
+        weather_info = return_weather_info(lowered[7:])
+        return weather_info
     else:
         return choice(['I do not understand...',
                        'What are you talking about?',
