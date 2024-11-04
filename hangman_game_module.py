729ddd4ed551856d2eebfc5cd95fb9c42248f81d
wiktor <wblaeznek@gmail.com>
2024-11-04 20:29:00 +0100

    adding hangman game to responses

--- hangman_game_module.py
+++ hangman_game_module.py
@@ -1,10 +1,51 @@
-from discord import Message
-
-from main import client
+from random import random
 
 
-def hangman_game():
-    @client.event
-    async def game_continuation(message: Message):
-        user_message: str = message.content
-        print(message)
+draw_list = [
+    f'/',
+    f'/|',
+    f'/|\\',
+    f' | \n/|\\ \n',
+    f' | \n | \n/|\\ \n',
+    f' | \n | \n | \n/|\\ \n',
+    f' | \n | \n | \n | \n/|\\ \n',
+    f' | \n | \n | \n | \n | \n/|\\ \n',
+    f' ________ \n | \n | \n | \n | \n | \n/|\\ \n',
+    f' ________ \n |      |\n | \n | \n | \n | \n/|\\ \n',
+    f' ________ \n |      |\n |      o\n | \n | \n | \n/|\\ \n',
+    f' ________ \n |      |\n |      o\n |     /|\\ \n | \n | \n/|\\ \n',
+    f' ________ \n |      |\n |      o\n |     /|\\ \n |     / \\ \n | \n/|\\ \n',
+]
+lives_lost = 0
+
+words_list = ["kot", "pies", "drzewo", "komputer", "klawiatura", "telefon", "okno", "słońce", "książka", "szkoła",
+              "dom", "biurko", "chmura", "jabłko", "telefon", "sztuka", "noc", "dzień", "morze", "ogródek",
+              "woda", "miasto", "muzyka", "obraz", "ogród", "kwiat", "samochód", "ulica", "słowo", "język",
+              "obiad", "kolacja", "rower", "deszcz", "śnieg", "sklep", "praca", "nauka", "uczeń", "nauczyciel",
+              "język", "twarz", "kawa", "herbata", "kanapka", "zima", "wiosna", "lato", "jesień", "papier",
+              "zeszyt", "stół", "okulary", "nóż", "widelec", "łyżka", "czapka", "szalik", "buty", "słuchawki",
+              "telewizja", "radio", "internet", "kabel", "myszka", "projekt", "piłka", "ręka", "nogi", "głowa",
+              "zegar", "pociąg", "samolot", "plaża", "jezioro", "człowiek", "dziecko", "wiatrak", "klimat",
+              "mikrofon", "głośnik", "księżyc", "zabawa", "miłość", "sztuka", "teatr", "film", "opowiadanie",
+              "powieść", "obraz", "zdjęcie", "matematyka", "fizyka", "chemia", "biologia", "geografia", "historia"]
+
+password = 'kot'
+
+user_guesses = []
+
+def hangman_game(guess: str):
+    guess.lower()
+    global lives_lost
+    global draw_list
+    global password
+    global user_guesses
+
+    if guess in password:
+        user_guesses += guess
+        return f'brawo, {guess} jest w haśle'
+    else:
+        lives_lost+=1
+        user_guesses += guess
+        print(draw_list[lives_lost-1])
+        return f'{guess} nie ma w haśle \n{draw_list[lives_lost-1]}'
+
--- responses.py
+++ responses.py
@@ -1,21 +1,29 @@
 from random import choice, randint
+from hangman_game_module import hangman_game
 
+in_game = False
 
 def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
-
-    if lowered == '':
-        return 'Well, you\'re awfully silent...'
-    elif 'hello' in lowered:
-
-        return 'Hello there!'
-    elif 'how are you' in lowered:
-        return 'Good, thanks!'
-    elif 'bye' in lowered:
-        return 'See you!'
-    elif 'roll dice' in lowered:
-        return f'You rolled: {randint(1, 6)}'
+    global in_game
+    if in_game:
+        result = hangman_game(user_input)
+        return f'{result}\n\nZgaduj:'
     else:
-        return choice(['I do not understand...',
-                       'What are you talking about?',
-                       'Do you mind rephrasing that?'])
+        if lowered == '':
+            return None
+        elif 'hangamn' in lowered:
+            in_game = True
+            return 'Okay, lets play:)\n\nZgaduj:'
+        elif 'hello' in lowered:
+            return 'Hello there!'
+        elif 'how are you' in lowered:
+            return 'Good, thanks!'
+        elif 'bye' in lowered:
+            return 'See you!'
+        elif 'roll dice' in lowered:
+            return f'You rolled: {randint(1, 6)}'
+        else:
+            return choice(['I do not understand...',
+                           'What are you talking about?',
+                           'Do you mind rephrasing that?'])
